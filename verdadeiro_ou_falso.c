0ba64d9b9d241ba9c6646ddc8930b0c50e2f46f7
Sílvia <94696788+sissi-24@users.noreply.github.com>
2022-08-10 23:38:54 -0300

    Create verdadeiro_ou_falso.c

---
+++ verdadeiro_ou_falso.c
@@ -0,0 +1,20 @@
+/*implemente um programa que solicite duas variaveis inteiras e em seguida aplique todos os operadores realacionais aos inteiros lidos
+(sendo 1 - verdade ou 0 - falso)*/
+include <stdio.h>
+
+int main() {
+    //definicao de variaveis
+    int a, b;
+    
+    //entrada de dados
+    printf("entre aqui com 2 numeros inteiros: \n");
+    scanf("%d %d", &a, &b);
+    
+    //processamento e saida de dados
+    printf("O valor relacional de %d == %d: %d\n", a, b, a==b);
+    printf("O valor relacional de %d < %d: %d\n", a, b, a<b);
+    printf("O valor relacional de %d > %d: %d\n", a, b, a>b);
+    printf("O valor relacional de %d >= %d: %d\n", a, b, a>=b);
+    printf("O valor relacional de %d <= %d: %d\n", a, b, a<=b);
+    printf("O valor relacional de %d != %d: %d\n", a, b, a!=b);
+}
