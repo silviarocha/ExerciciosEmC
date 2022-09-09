c1278cb752149faa4721281da21d96f3a2aed3ba
Sílvia <94696788+silviarocha@users.noreply.github.com>
2022-09-09 18:24:35 -0300

    Create CALCULOARITMETICO(MENU).c
    EXERCICIO EM DESENVOLVIMENTO

---
+++ CALCULOARITMETICO(MENU).c
@@ -0,0 +1,9 @@
+/*Fazer um programa em "C" que solicite 2 números e informe:
+ a) A soma dos números;
+ b) O produto do primeiro número pelo quadrado do segundo;
+ c) O quadrado do primeiro número. */
+#include <stdio.h>
+int main() {
+    //definicao das variaveis
+    
+}
