b0c8bab25411142f812f89f2cb7193aa2b5eb2bf
Sílvia <94696788+silviarocha@users.noreply.github.com>
2022-09-14 21:14:02 -0300

    Create senha_q2.c
    questao em desenvolvimento

---
+++ senha_q2.c
@@ -0,0 +1,4 @@
+/* escreva um prog em c que leia um num menor que 9999 representando uma senha. o prog deve verificar se a senha e valida (imprimir "senha valida") ou se a senha viola alguma das seguintes regras caso em que o programa devera apresentar a mensagem de violacao correspondente:
+regras e mensagens: 1- algarismos iguais: a senha eh formada por algarismos iguais;
+2- sequencia crescente: a senha eh formada por uma sequencia crescente;
+3- sequencia decrescente: a senha eh formada por uma sequencia decrescente */
