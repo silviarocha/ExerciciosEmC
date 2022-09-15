ff17ccaaf10bab025b7a17622c1479f366fd496e
Sílvia <94696788+silviarocha@users.noreply.github.com>
2022-09-14 21:03:31 -0300

    Create q27_lista.c
    esta em andamento

---
+++ q27_lista.c
@@ -0,0 +1,20 @@
+/* 27. Escreva um programa que leia 3 números e calcule a média ponderada entre eles. 
+Considere que o maior número recebe peso 5 e os outros dois recebem peso 2,5.*/
+#include <stdio.h>
+
+int main() {
+   float num1, num2, num3, media1, media2, media3, mediaPonderada;
+   
+    printf(" entre com o primeiro numero: \n");
+   scanf("%f", &num1);
+    printf(" entre com o segundo numero: \n");
+   scanf("%f", &num2);
+     printf("entre com o terceiro numero: \n");
+   scanf("%f", &num3);
+   
+   media1 = 5.0;
+   media2 = 2.5;
+   media3 = 2.5;
+   
+   if(num1 > )
+}
