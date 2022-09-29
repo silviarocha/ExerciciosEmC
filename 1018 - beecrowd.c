116e44672d7b370579bd093d673dac7ef01d68b3
Sílvia <94696788+silviarocha@users.noreply.github.com>
2022-09-28 21:13:00 -0300

    Update 1018 - beecrowd.c

--- 1018 - beecrowd.c
+++ 1018 - beecrowd.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-
-int main()
-{
-  int n;
+ 
+int main() {
+ int n;
   
   scanf("%d", &n);
   
+  printf("%d\n", n);
   printf("%d nota(s) de R$ 100,00\n", n/100);
   n = n % 100;
   printf("%d nota(s) de R$ 50,00\n", n/50);
@@ -19,4 +19,5 @@ int main()
   printf("%d nota(s) de R$ 2,00\n", n/2);
   n = n % 2;
   printf("%d nota(s) de R$ 1,00\n", n);
+    return 0;
 }
