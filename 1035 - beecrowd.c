c2babaeb9fd47bd90022f5f9af61fcc3a7c805b8
Sílvia <94696788+silviarocha@users.noreply.github.com>
2022-10-09 18:14:20 -0300

    Create 1035 - beecrowd.c

---
+++ 1035 - beecrowd.c
@@ -0,0 +1,16 @@
+/*Leia 4 valores inteiros A, B, C e D. A seguir, se B for maior do que C e se D for maior do que A,
+e a soma de C com D for maior que a soma de A e B e se C e D, ambos, forem positivos e se a variável A for par escrever a mensagem "Valores aceitos", 
+senão escrever "Valores nao aceitos".*/
+#include <stdio.h>
+
+int main() {
+    int a, b, c, d;
+    
+    scanf("%d%d%d%d", &a, &b, &c, &d);
+    
+    if((b > c) && (d > a) && (c + d) > (a + b) && c <= d < a){
+        printf("Valores aceitos");
+    } else{
+        printf("Valores nao aceitos");
+    }
+}
