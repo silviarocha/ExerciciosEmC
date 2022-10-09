841e81bdbeea46045c2d63f844524a12560df5fd
Sílvia <94696788+silviarocha@users.noreply.github.com>
2022-10-09 18:41:19 -0300

    Update 1035 - beecrowd.c

--- 1035 - beecrowd.c
+++ 1035 - beecrowd.c
@@ -8,7 +8,7 @@ int main() {
     
     scanf("%d%d%d%d", &a, &b, &c, &d);
     
-    if((b > c) && (d > a) && (c + d) > (a + b) && c <= d < a){
+       if((b > c) && (d > a) && ((c + d) > (a + b)) && c > 0 && d > 0 && a%2==0){
         printf("Valores aceitos");
     } else{
         printf("Valores nao aceitos");
