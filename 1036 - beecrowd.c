0e515c97cce507e550398a9e2cffe0d1406c0b5e
Sílvia <94696788+silviarocha@users.noreply.github.com>
2022-10-09 18:32:15 -0300

    Create 1036 - beecrowd.c

---
+++ 1036 - beecrowd.c
@@ -0,0 +1,17 @@
+/*Leia 3 valores de ponto flutuante e efetue o cálculo das raízes da equação de Bhaskara. Se não for possível calcular as raízes, 
+mostre a mensagem correspondente “Impossivel calcular”, caso haja uma divisão por 0 ou raiz de numero negativo.*/
+#include <stdio.h>
+#include <math.h>
+int main() {
+    double a, b, c, delta, R1, R2;
+    
+    scanf("%lf%lf%lf", &a, &b, &c);
+    
+    // formula de bhaskara : x = -b +- raiz quad delta/2xa
+    // delta => b²-4xaxc
+    
+    delta = b*b - 4*a*c;
+    R1 = -b + (sqrt)
+}
+
+//esta sendo feita ainda
