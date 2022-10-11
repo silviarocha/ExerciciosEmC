bb4b9f32cf8872f5d0ad9a7e58ff3976364d8761
Sílvia <94696788+silviarocha@users.noreply.github.com>
2022-10-11 20:12:03 -0300

    Update 1036 - beecrowd.c

--- 1036 - beecrowd.c
+++ 1036 - beecrowd.c
@@ -2,16 +2,23 @@
 mostre a mensagem correspondente “Impossivel calcular”, caso haja uma divisão por 0 ou raiz de numero negativo.*/
 #include <stdio.h>
 #include <math.h>
-int main() {
-    double a, b, c, delta, R1, R2;
-    
-    scanf("%lf%lf%lf", &a, &b, &c);
-    
-    // formula de bhaskara : x = -b +- raiz quad delta/2xa
-    // delta => b²-4xaxc
-    
-    delta = b*b - 4*a*c;
-    R1 = -b + (sqrt)
-}
 
-//esta sendo feita ainda
+int main (){
+	double a, b, c, r1, r2, delta;
+	
+	scanf("%lf%lf%lf", &a, &b, &c);
+	
+	//x = -b +- sqrt(pow(b, 2) - 4 * a * c) / 2 * a
+	
+	delta = pow(b,2) - 4*a*c;
+	
+	r1 = b*(-1) + sqrt(delta) / (2*a);
+	r2 = b*(-1) - sqrt(delta) / (2*a);
+	
+	if (delta > 0){
+	printf("R1 = %.5lf\n", r1);
+	printf("R2 = %.5lf\n", r2);
+	} if (delta <= 0){
+		printf("Impossivel calcular");
+	}
+}
