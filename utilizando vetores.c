19a40d926f4f3de0191d446cb83ceaef32380b74
Sílvia <94696788+silviarocha@users.noreply.github.com>
2022-11-15 20:16:45 -0300

    Create utilizando vetores.c

---
+++ utilizando vetores.c
@@ -0,0 +1,18 @@
+/* exemplo de vetor */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int numero[1000];
+    int contador;
+    int total;
+    
+    contador = 0;
+    
+    printf ("entre com um numero (escreva -1 para interromper): \n");
+    scanf ("%i", &numero[contador]);
+    
+    while (numero[contador-1] != -1){
+        total = contador
+    }
+}
