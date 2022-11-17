b38cd993755b976e691a76fd509d089634a16475
Sílvia <94696788+silviarocha@users.noreply.github.com>
2022-11-16 21:24:55 -0300

    Update utilizando vetores.c
    criado 15.11.2022
    atualizado 16.11.2022

--- utilizando vetores.c
+++ utilizando vetores.c
@@ -3,16 +3,20 @@
 #include <stdlib.h>
 
 int main() {
-    int numero[1000];
-    int contador;
+      int numero[1000];
+    int contador = 0;
     int total;
     
-    contador = 0;
-    
-    printf ("entre com um numero (escreva -1 para interromper): \n");
+    do{
+    printf ("entre com um numero (escreva -999 para interromper): \n");
     scanf ("%i", &numero[contador]);
+        contador++;
+    } while (numero[contador-1] != -999);
+    total = contador-1;
     
-    while (numero[contador-1] != -1){
-        total = contador
+    printf ("os numeros que voce digitou foram: \n ");
+    for (contador = 0; contador < numero[contador]; contador++){
+        printf (" %d \n", numero[contador]);
     }
+    return 0;
 }
